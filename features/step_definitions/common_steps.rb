894e4b9b6b29c3292f7c1748e41941247b210e73
Igor Dorovskikh <i.dorovskikh@agilefusion.com>
2016-07-21 16:45:13 -0700

    Revert "adding creat new and get existing test users"
    This reverts commit e79a1252e472e7872fae5a182c97848b0590f4a3.

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -1,26 +1,5 @@
-Given(/^get app access token$/) do
-steps %Q{
-    Given a "GET" request is made to "/oauth/access_token"
-    When these parameters are supplied in URL:
-    |grant_type    | client_credentials               |
-    |client_id     | 931957476934241                  |
-    |client_secret | 68b3a63cfb0a6e961978ddd205e005f2 |
-    Then the api call should succeed
-    And value of access token is saved in a global variable
-}
-end
-
 Given(/^a "([^"]*)" request is made to "([^"]*)"$/) do |method, path|
   # constructs URI with endpoint i.e http://api.bayqatraining.com/user
-
-  if path.include?('{')
-    path_variable = path[/{(.+)}/, 1]
-    if FACEBOOK_STORE.has_key? path_variable
-      path.gsub!(/{.*}/, FACEBOOK_STORE[path_variable])
-    end
-
-  end
-
   @uri    = URI("#{$uri_hostname}#{path}")
   # @method is a variable that reference to the verb (i.e GET, POST)
   @method = method
@@ -29,11 +8,7 @@ end
 When(/^these parameters are supplied in URL:$/) do |table|
   # flattens table parameters from feature file
   # into URI format i.e grant_type=client_credentials&client_id=23443334
-  request_parameters = table.rows_hash
-  if request_parameters.has_key? "access_token"
-    request_parameters["access_token"] = @app_access_token
-  end
-    @uri.query = URI.encode_www_form(request_parameters)
+    @uri.query = URI.encode_www_form(table.rows_hash)
 end
 
 Then(/^the api call should (succeed|fail)$/) do |condition|
@@ -84,14 +59,5 @@ Then(/^the api call should (succeed|fail)$/) do |condition|
 end
 
 And(/^value of access token is saved in a global variable$/) do
-   @app_access_token = @response_body.split("=").last
-end
-
-And(/^I save all users ids$/) do
-  @ids = Array.new
-  body = JSON.parse(@response_body)
-  body["data"].each do |user|
-    @ids << user["id"]
-  end
-  p @ids
+  puts @access_token = @response_body.split("=").last
 end
--- features/support/hooks.rb
+++
@@ -1,3 +0,0 @@
-Before do
-  step "get app access token"
-end

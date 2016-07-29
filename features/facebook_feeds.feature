db6bd16a7a07db5fc49dbb5c58169f35240d07af
Igor Dorovskikh <i.dorovskikh@agilefusion.com>
2016-07-28 17:29:23 -0700

    parameterizing assertions

--- features/facebook_feeds.feature
+++ features/facebook_feeds.feature
@@ -3,6 +3,8 @@ Feature: post and read feeds
 
   Scenario: As facebook developer, I publish a new feed on test user profile
     Given I create test user1
+    Given I create test user2
+
     Given a request is made to "/{user1}/feed"
     When these parameters are supplied in URL:
       |installed                | true              |
@@ -14,10 +16,11 @@ Feature: post and read feeds
     When these parameters are supplied in URL:
       |user_access_token        |                   |
     Then the GET api call should succeed
-    And these response keys should have value:
+
+    And these response keys should have value for "data":
       |message                  | test              |
 
-    And these response keys should not be nil:
+    And these response keys should not be nil for "data":
       | created_time |
       | id           |
 
--- features/step_definitions/response_steps.rb
+++ features/step_definitions/response_steps.rb
@@ -1,4 +1,4 @@
-And(/^these response keys should have value:$/) do |table|
+And(/^these response keys should have value for "([^"]*)":$/) do |key, table|
   # Parse the JSON document _source_ into a Ruby HashMap data structure and return it
   # i.e table = {"id"=>108, "name"=>"Andrei", "email"=>"andrei9@gmail.com"}
 
@@ -6,11 +6,11 @@ And(/^these response keys should have value:$/) do |table|
     # go through each row in table from feature file
     # match that first row from server response is equal to first row from table in feature file
     # name"=>"Andrei" == "name", "Andrei"
-    expect(json_parse['data'][0][row[0]]).to be == row[1]
+    expect(json_parse[key][0][row[0]]).to be == row[1]
   end
 end
 
-Then /^these response keys (should|should not) be nil:$/ do |should, table|
+Then /^these response keys (should|should not) be nil for "([^"]*)":$/ do |should, key, table|
 
   if should == 'should not'
     condition = :not_to
@@ -20,6 +20,6 @@ Then /^these response keys (should|should not) be nil:$/ do |should, table|
   end
 
   table.raw.each do |row|
-     expect(json_parse['data'][0][row[0]]).send(condition, be_nil)
+     expect(json_parse[key][0][row[0]]).send(condition, be_nil)
   end
 end

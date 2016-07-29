4be3d95f017b5fc640378c45b83ab1f04ebee859
Igor Dorovskikh <i.dorovskikh@agilefusion.com>
2016-07-28 17:53:30 -0700

    fixing comment

--- features/step_definitions/response_steps.rb
+++ features/step_definitions/response_steps.rb
@@ -1,11 +1,11 @@
 And(/^these response keys should have value for "([^"]*)":$/) do |key, table|
   # Parse the JSON document _source_ into a Ruby HashMap data structure and return it
-  # i.e table = {"id"=>108, "name"=>"Andrei", "email"=>"andrei9@gmail.com"}
+  # i.e table = {"id"=>108, "name"=>"Igor", "email"=>"igor@gmail.com"}
 
   table.raw.each do |row|
     # go through each row in table from feature file
     # match that first row from server response is equal to first row from table in feature file
-    # name"=>"Andrei" == "name", "Andrei"
+    # name"=>"igor" == "name", "igor"
     expect(json_parse[key][0][row[0]]).to be == row[1]
   end
 end
